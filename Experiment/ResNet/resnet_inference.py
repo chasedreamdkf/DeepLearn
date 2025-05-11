7daa65bfd0bec9e52a0bf5fcceaac4af2bfaf2d9
chasedreamdkf <3607068551@qq.com>
2025-05-11 11:37:24 +0800

    解决部分报错

--- Experiment/ResNet/resnet_inference.py
+++ Experiment/ResNet/resnet_inference.py
@@ -78,7 +78,11 @@ def get_model(m_path, vis_model=False):
 
 
 if __name__ == "__main__":
-
+    # 解决
+    """ OMP: Error #15: Initializing libiomp5md.dll, but found libiomp5md.dll already initialized.
+        OMP: Hint This means that multiple copies of the OpenMP runtime have been linked into the program. That is dangerous, since it can degrade performance or cause incorrect results. The best thing to do is to ensure that only a single OpenMP runtime is linked into the process, e.g. by avoiding static linking of the OpenMP runtime in any library. As an unsafe, unsupported, undocumented workaround you can set the environment variable KMP_DUPLICATE_LIB_OK=TRUE to allow the program to continue to execute, but that may cause crashes or silently produce incorrect results. For more information, please see http://www.intel.com/software/products/support/.
+    """
+    os.environ["KMP_DUPLICATE_LIB_OK"]="True"
     img_dir = os.path.join("data/hymenoptera_data/val/bees")
     model_path = "./checkpoint_14_epoch.pkl"
     time_total = 0
